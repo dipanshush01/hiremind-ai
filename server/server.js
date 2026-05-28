c5ed3178845be295006fbb30aa2beed8414831ef
dipanshush01 <144903863+dipanshush01@users.noreply.github.com>
2026-05-28 14:14:55 +0530

    Fix CORS

--- server/server.js
+++ server/server.js
@@ -39,6 +39,8 @@ app.use(
   })
 );
 
+app.use("/api/auth", authRoutes);
+
 // Body parsing
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
